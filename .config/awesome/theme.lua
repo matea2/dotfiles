29c01ae506c8f3c09d86efe75d71014165c9beaf
matea2 (desktop) <matea.im+github@gmail.com>
2017-05-20 18:20:15 +0900

    フォントの設定

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -1,6 +1,6 @@
 local theme = dofile("/usr/share/awesome/themes/zenburn/theme.lua")
 
-theme.font = "M+ 2m 9"
+theme.font = "Noto Sans Mono CJK JP 9"
 theme.menu_height = 20
 theme.menu_width = 150
 
